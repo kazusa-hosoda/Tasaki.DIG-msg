1cd114395d35b1f21d6948501ac52720db9fa81f
細田 一冴 <131834448+kazusa-hosoda@users.noreply.github.com>
2023-07-07 08:33:57 +0900

    Create 田崎.js

---
+++ 田崎.js
@@ -0,0 +1,21 @@
+window.addEventListener("scroll", function () {
+
+    const elm = document.querySelector(".elm");
+
+    const scroll = window.pageYOffset;
+
+    if (scroll > 500) {
+
+        elm.style.opacity = "1";
+
+        // console.log(scroll);
+
+    } else {
+
+        elm.style.opacity = "0";
+
+        // console.log(scroll);
+
+    }
+
+});
